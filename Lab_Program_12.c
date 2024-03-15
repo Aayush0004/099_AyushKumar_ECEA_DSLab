a2851e8827a7da5086d8aed74eb6df18d47af783
Aayush0004 <149601246+Aayush0004@users.noreply.github.com>
2024-03-15 22:15:32 +0530

    Update Lab_Program_12.c

--- Lab_Program_12.c
+++ Lab_Program_12.c
@@ -6,10 +6,9 @@ Enrollment No.-23DOECBTEC000099
 */
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
 
 typedef struct Node {
-    int data;
+    char data;
     struct Node* next;
 } Node;
 
@@ -17,7 +16,7 @@ typedef struct {
     Node* top;
 } Stack;
 
-Node* createNode(int data) {
+Node* createNode(char data) {
     Node* newNode = (Node*)malloc(sizeof(Node));
     if (newNode == NULL) {
         printf("Memory allocation failed!\n");
@@ -36,71 +35,59 @@ int isEmpty(Stack* s) {
     return (s->top == NULL);
 }
 
-void push(Stack* s, int data) {
+void push(Stack* s, char data) {
     Node* newNode = createNode(data);
     newNode->next = s->top;
     s->top = newNode;
 }
 
-int pop(Stack* s) {
+char pop(Stack* s) {
     if (isEmpty(s)) {
         printf("Stack Underflow!\n");
         exit(EXIT_FAILURE);
     }
-    int data = s->top->data;
+    char data = s->top->data;
     Node* temp = s->top;
     s->top = s->top->next;
     free(temp);
     return data;
 }
 
-int evaluatePostfix(char* postfix) {
-    Stack operandStack;
-    initialize(&operandStack);
+int areBracketsBalanced(char* exp) {
+    Stack bracketStack;
+    initialize(&bracketStack);
 
-    int i = 0;
-    while (postfix[i] != '\0') {
-        if (isdigit(postfix[i])) {
-            push(&operandStack, postfix[i] - '0');
-        } else {
-            int operand2 = pop(&operandStack);
-            int operand1 = pop(&operandStack);
-            switch (postfix[i]) {
-                case '+':
-                    push(&operandStack, operand1 + operand2);
-                    break;
-                case '-':
-                    push(&operandStack, operand1 - operand2);
-                    break;
-                case '*':
-                    push(&operandStack, operand1 * operand2);
-                    break;
-                case '/':
-                    if (operand2 != 0)
-                        push(&operandStack, operand1 / operand2);
-                    else {
-                        printf("Division by zero error!\n");
-                        exit(EXIT_FAILURE);
-                    }
-                    break;
-                default:
-                    printf("Invalid operator!\n");
-                    exit(EXIT_FAILURE);
+    for (int i = 0; exp[i] != '\0'; i++) {
+        if (exp[i] == '(' || exp[i] == '{' || exp[i] == '[') {
+            push(&bracketStack, exp[i]);
+        } else if (exp[i] == ')' || exp[i] == '}' || exp[i] == ']') {
+            if (isEmpty(&bracketStack)) {
+                return 0;
+            } else {
+                char topBracket = pop(&bracketStack);
+                if ((exp[i] == ')' && topBracket != '(') ||
+                    (exp[i] == '}' && topBracket != '{') ||
+                    (exp[i] == ']' && topBracket != '[')) {
+                    return 0;
+                }
             }
         }
-        i++;
     }
 
-    return pop(&operandStack);
+    return isEmpty(&bracketStack);
 }
 
 int main() {
-    char postfix[100];
+    char exp[100];
 
-    printf("Enter a postfix expression: ");
-    fgets(postfix, sizeof(postfix), stdin);
-    int result = evaluatePostfix(postfix);
-    printf("Result: %d\n", result);
+    printf("Enter an expression: ");
+    fgets(exp, sizeof(exp), stdin);
+
+    if (areBracketsBalanced(exp)) {
+        printf("Brackets are balanced!\n");
+    } else {
+        printf("Brackets are not balanced!\n");
+    }
 
     return 0;
 }
